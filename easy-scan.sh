223eb0a5d285b7063beb04c89ca5f070d8a22ef3
AlexLinov <74632540+AlexLinov@users.noreply.github.com>
2024-06-02 10:54:52 -0400

    Update easy-scan.sh
    Added ability to filter out closed/filtered ports that are not applicable across the entire scan.

--- easy-scan.sh
+++ easy-scan.sh
@@ -8,6 +8,7 @@ fi
 ips_input_file="$1"
 open_ports_scan_output="open-ports-scan.scan"
 custom_scan_output="custom-scan.scan"
+filtered_scan_output="filtered-custom-scan.txt"
 
 echo -e "\033[0;32mPerforming initial scan to find open ports...\033[0m"
 nmap --open -p- -iL "$ips_input_file" -oG "$open_ports_scan_output" -vv
@@ -26,6 +27,10 @@ echo -e "\033[0;31mOpen ports found:\033[0m $open_ports"
 sleep 5
 echo -e "\033[0;32mPerforming full scan on found ports...\033[0m"
 
-nmap -iL "$ips_input_file" -p"$open_ports" -sCV -A -oN "$custom_scan_output" -vv
+nmap -iL "$ips_input_file" -p"$open_ports" -sCV -A -oA "$custom_scan_output" -vv
 
-echo -e "\033[0;32mFull scan completed. Results are saved in\033[0m '$custom_scan_output'."
+echo -e "\033[0;32mFiltering out non-open ports from the scan results...\033[0m"
+
+grep -vE "filtered|closed" "$custom_scan_output.nmap" > "$filtered_scan_output"
+
+echo -e "\033[0;32mFiltered scan completed. Results are saved in\033[0m '$filtered_scan_output'."
